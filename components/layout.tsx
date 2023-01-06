ddbb09139f7c6c22fedc8b90db42f6b9552df687
syrius527 <djlee1031@naver.com>
2023-01-07 01:02:05 +0900

    common layout first setting

---
+++ components/layout.tsx
@@ -0,0 +1,20 @@
+import Header from "./header";
+import Footer from "./footer";
+
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Header />
+      <div>
+        {children}
+      </div>
+      <Footer />
+    </>
+  );
+}
